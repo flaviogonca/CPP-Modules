72e8964ed6ffe3ba94d9af3fa1620b145e1a3eb0
Flávio Gonçalves <thisflavio09@gmail.com>
2025-04-28 19:08:03 +0100

    initializing the classes of ex01

---
+++ CPP-Module-00/ex01/Contact.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmalungo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 17:33:27 by fmalungo          #+#    #+#             */
+/*   Updated: 2025/04/28 17:33:31 by fmalungo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef Contact_HPP
+# define Contact_HPP
+
+class Contact {
+    private:
+        std::string firstName;
+        std::string lastName;
+        std::string nickName;
+        std::string phoneNumber;
+        std::string darkestScret;
+}
+
+#endif
