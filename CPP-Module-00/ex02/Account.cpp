8d9eda291211db4d3d6b9d277f06f1256b5edf28
Flávio Gonçalves <thisflavio09@gmail.com>
2025-05-08 15:47:06 +0100

    In this commit is added the required for the subject which is to remake the Account.cpp, and the output must be like the logfile given, except for the timestamp which is going to be differente becouse of the actual time.

--- CPP-Module-00/ex02/Account.cpp
+++ CPP-Module-00/ex02/Account.cpp
@@ -10,4 +10,91 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+#include <iostream>
+#include <ctime>
 
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+void	Account::_displayTimestamp( void )
+{
+    std::time_t now = time(0);
+    std::tm* local = localtime(&now);
+    char buffer[20];
+
+    std::strftime(buffer, sizeof(buffer), "[%Y%m%d_%H%M%S] ", local);
+    std::cout << buffer;
+}
+
+Account::Account( int initial_deposit )
+{
+    _nbAccounts++;
+    _totalAmount += initial_deposit;
+    _nbDeposits = 0;
+    _nbWithdrawals = 0;
+
+    _amount = initial_deposit;
+    _accountIndex = _nbAccounts - 1;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+}
+
+void Account::displayStatus() const
+{
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+Account::~Account (void)
+{
+
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+    _totalAmount -= _amount;
+    _nbAccounts--;
+}
+
+int	Account::getNbAccounts( void ){    return _nbAccounts;     }
+
+int	Account::getTotalAmount( void ){    return _totalAmount;     }
+
+int	Account::getNbDeposits( void ){    return _totalNbDeposits;     }
+
+int	Account::getNbWithdrawals( void ){    return _totalNbWithdrawals;     }
+
+void Account::displayAccountsInfos( void )
+{
+    _displayTimestamp();
+    std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount() << ";deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+void	Account::makeDeposit( int deposit )
+{
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";deposit:" << deposit ;
+    _amount += deposit;
+    _nbDeposits++;
+    _totalNbDeposits++;
+    _totalAmount += deposit;
+    std::cout << ";amount:" << _amount << ";nb_deposits:" << _nbDeposits << std::endl;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";p_amount:" << _amount;
+    if (_amount < withdrawal)
+    {
+        std::cout << ";withdrawal:refused" << std::endl;
+        return false;
+    }
+    _amount -= withdrawal;
+    _nbWithdrawals++;
+    _totalNbWithdrawals++;
+    _totalAmount -= withdrawal;
+    std::cout << ";withdrawal:" << withdrawal << ";amount:" << _amount << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+    return true;
+}
