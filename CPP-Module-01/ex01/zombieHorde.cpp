b94d617114e9b12dc2f1259fea833d5e2990ca14
Flávio Gonçalves <thisflavio09@gmail.com>
2025-05-09 11:04:57 +0100

    Init ex01 and remove the memory lib from ex00 becouse was not used

--- CPP-Module-01/ex01/zombieHorde.cpp
+++ CPP-Module-01/ex01/zombieHorde.cpp
@@ -1,35 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fmalungo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/09 08:27:40 by fmalungo          #+#    #+#             */
-/*   Updated: 2025/05/09 08:27:42 by fmalungo         ###   ########.fr       */
+/*   Created: 2025/05/09 10:54:26 by fmalungo          #+#    #+#             */
+/*   Updated: 2025/05/09 10:54:29 by fmalungo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIE_HPP
-# define ZOMBIE_HPP
 
-#include <iostream>
-
-#define RED "\033[31m"
-#define RESET "\033[0m"
-
-class Zombie
-{
-    private:
-        std::string name;
-
-    public:
-        Zombie(std::string name);
-        ~Zombie();
-
-        void announce( void );
-        Zombie* newZombie( std::string name );
-        void randomChump( std::string name );
-};
-
-#endif
