3015ba97070ec054996efe23cfe9597126841bad
Flávio Gonçalves <thisflavio09@gmail.com>
2025-05-14 17:16:21 +0100

    Create ex05 files and Harl header class

---
+++ CPP-Module-01/ex05/Harl.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmalungo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 16:52:26 by fmalungo          #+#    #+#             */
+/*   Updated: 2025/05/14 16:52:29 by fmalungo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void Harl::debug( void )
+{
+}
+
+void Harl::info( void )
+{
+}
+
+void Harl::warning( void )
+{
+}
+
+void Harl::error( void )
+{
+}
+
+void Harl::complain( std::string level )
+{
+}
---
+++ CPP-Module-01/ex05/Harl.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmalungo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 16:52:32 by fmalungo          #+#    #+#             */
+/*   Updated: 2025/05/14 16:52:34 by fmalungo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+    private:
+        void debug( void );
+        void info( void );
+        void warning( void );
+        void error( void );
+
+    public:
+        Harl(/* args */);
+        ~Harl();
+        void complain( std::string level );
+};
+
+Harl::Harl(/* args */)
+{
+}
+
+Harl::~Harl()
+{
+}
+
+
+#endif
