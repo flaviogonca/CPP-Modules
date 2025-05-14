34ea7e17515a89c38c9f4642a67d3b52a39c75dc
Flávio Gonçalves <thisflavio09@gmail.com>
2025-05-14 16:49:34 +0100

    This commit implements the following feat: Open the file <filename> and copy its content into a new file <filename>.replace, replacing every occurrence of s1 with s2.

--- CPP-Module-01/ex04/main.cpp
+++ CPP-Module-01/ex04/main.cpp
@@ -20,29 +20,40 @@ int main(int ac, char **av)
         std::cerr << "Try: <filename> <stringToSearch> <stringToReplace>" << std::endl;
         return 1;
     }
+    std::string str1 = av[2];
+    std::string str2 = av[3];
+    std::string filename = av[1];
+    if (str1.empty() || str2.empty() || filename.empty())
+    {
+        std::cerr << "<filename> <stringToSearch> <stringToReplace> Must not be empty" << std::endl;
+        return 1;
+    }
     std::fstream file(av[1], std::ios::in);
     if (!file)
     {
-        std::cerr << "Could Not find/open <" << av[1] << "> file, try again" << std::endl;
+        std::cerr << "Could Not find/open <" << filename << "> file, try again" << std::endl;
         return 1;
     }
-    if (file.eof())
-    {
-        std::cerr << "Empty file" << std::endl;
-        return 1;
-    }
-    std::string newfile = av[1];
-    std::fstream file1(newfile.append(".replace"), std::ios::out);
+    std::string line;
+    std::string content;
+    std::cout << content;
+    std::fstream file1(filename.append(".replace"), std::ios::out);
+    while (std::getline(file, line))
+        content += line + '\n';
     if (!file1)
     {
-        std::cerr << "Could Not create <" << av[1] << ".replace > file, try again" << std::endl;
+        std::cerr << "Could Not create <" << filename << ".replace > file, try again" << std::endl;
         return 1;
     }
-    
-    std::string line;
-    while (std::getline(file, line))
+    size_t i = -1;
+    while (++i < content.size() - 1)
     {
-        file1 << line << std::endl;
+        if (content.at(i) == str1.at(0) && str1.compare(content.substr(i,str1.length())) == 0)
+        {
+            file1 << str2;
+            i += str1.length();
+        }
+        file1 << content.at(i);
     }
     file1.close();
     file.close();
